044ecebd9ccd5b98434c4494af535eda8199b561
Pedro <pedro@storkandcrow.com>
2020-04-10 15:12:31 +0200

    feat: added noble-secp256k1
    added noble-secp256k1 (https://github.com/paulmillr/noble-secp256k1)
    removed all functions from main except lib_buffer, lib_keccak
    modified lib_account to make use of the new secp256k1 lib

--- example.js
+++ example.js
@@ -1,15 +1,19 @@
-const ethsig = require(".");
+const ethsig = require("./main");
 
-var message      = "Hello!";
-var private_key  = "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
-var address      = ethsig.addressFromKey(private_key);
-var message_hash = ethsig.keccak(message);
-var signature    = ethsig.signMessage(message, private_key);
-var signer       = ethsig.signerAddress(message, signature);
+let runTest = async () => {
+    var message      = "Hello!";
+    var private_key  = "0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef";
+    var address      = ethsig.addressFromKey(private_key);
+    var message_hash = ethsig.keccak(message);
+    var signature    = await ethsig.signMessage(message_hash, private_key);
+    var signer       = ethsig.signerAddress(message_hash, signature);
 
-console.log("private_key  :", private_key);
-console.log("address      :", address);
-console.log("message      :", message);
-console.log("message_hash :", message_hash);
-console.log("signature    :", signature);
-console.log("Verified?", signer === address);
+    console.log("private_key  :", private_key);
+    console.log("address      :", address);
+    console.log("message      :", message);
+    console.log("message_hash :", message_hash);
+    console.log("signature    :", signature);
+    console.log("Verified?", signer === address);
+}
+
+runTest();
